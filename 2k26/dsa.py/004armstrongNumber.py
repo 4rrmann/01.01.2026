5acccce26787dacf99af430e427ae2b370bd4650
Arman Ahmad <aru44362@gmail.com>
2026-01-01 15:38:34 +0530

    Add files via upload

---
+++ 2k26/dsa.py/004armstrongNumber.py
@@ -0,0 +1,27 @@
+n = int(input("enter a number: "))
+
+num = n
+result = 0
+
+count = len(str(n))
+# print(f"{count} digits")
+# count = 0
+
+# while num>0:
+#     count +=1
+#     num = num//10
+
+# print(f"{count} digits")
+while num>0:
+    last_digit = num%10
+    result = result + last_digit**count
+    num = num//10
+
+if (n == result):
+    print(f"yessir an armstrongNumber")
+else:
+    print(f"{result}? nah")
+
+    
+# time Complexity: O(log base(10) n)
+# space Complexity: O(1) --> constant space
